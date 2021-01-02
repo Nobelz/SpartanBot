635d5093ecaa82c9b9fb9c468456f212f72547f4
Nobelium <nobelzhou19@gmail.com>
2021-01-02 16:04:42 -0500

    Added average and running rating to rmp command

--- cogs/ratemyprofessor.py
+++ cogs/ratemyprofessor.py
@@ -1,9 +1,10 @@
 import json
 import ratemyprofessor
-import matplotlib.pyplot as plt
 import discord
+import matplotlib.pyplot as plt
 
 from discord.ext import commands
+from matplotlib.patches import Patch
 
 
 class RateMyProfessor(commands.Cog):
@@ -45,7 +46,15 @@ class RateMyProfessor(commands.Cog):
                 ratings = professor.get_ratings()
                 ratings.sort(reverse=True)
 
-                dates = [r.date for r in ratings[4:]]
+                dates = [r.date for r in ratings]
+                running_dates = [r.date for r in ratings[4:]]
+                helpful_ratings = []
+
+                i = 0
+                while i < len(ratings):
+                    average = [r.rating for r in ratings[0: i + 1]]
+                    helpful_ratings.append(sum(average) / (i + 1))
+                    i += 1
 
                 i = 0
                 running_averages = []
@@ -54,18 +63,23 @@ class RateMyProfessor(commands.Cog):
                     running_averages.append(sum(average) / 5)
                     i += 1
 
-                plt.plot(dates, running_averages, color='blue')
-                plt.title(f'Running Average for {professor.name}', fontsize=15)
+                plt.plot(running_dates, running_averages, color='blue')
+                plt.plot(dates, helpful_ratings, color='red')
+
+                plt.title(f'Ratings for {professor.name}', fontsize=15)
                 plt.ylabel('Rating', fontsize=12)
                 plt.xlabel('Date', fontsize=12)
                 plt.ylim(1.0, 5.0)
+                plt.legend(handles=[Patch(color='red', label='Average Rating'),
+                                    Patch(color='blue', label='Running Average Rating')])
                 plt.grid(True)
+
                 plt.savefig('graph.png')
                 plt.clf()
                 await ctx.send(file=discord.File('graph.png'))
 
     # test command
-    @commands.command()
+    """@commands.command()
     async def test(self, ctx, *, prof):
         with open('config.json', 'r') as f:
             configs = json.load(f)
@@ -76,17 +90,17 @@ class RateMyProfessor(commands.Cog):
         if professor is None:
             await ctx.send("Professor not found. Please try again.")
         else:
-            message = f"Professor name: {professor.name}\n"
+            message = f"Professor name: **{professor.name}**\n"
 
             if professor.rating != 0:
-                message += f"Professor rating: {professor.rating} / 5.0\n"
+                message += f"Professor rating: **{round(professor.rating, 1)}** / 5.0\n"
             else:
-                message += f"Professor rating: N/A\n"
+                message += "Professor rating: **N/A**\n"
 
             if professor.difficulty != 0:
-                message += f"Professor difficulty: {professor.difficulty} / 5.0"
+                message += f"Professor difficulty: **{round(professor.difficulty, 1)}** / 5.0"
             else:
-                message += f"Professor difficulty: N/A"
+                message += "Professor difficulty: **N/A**"
 
             await ctx.send(message)
 
@@ -94,7 +108,15 @@ class RateMyProfessor(commands.Cog):
                 ratings = professor.get_ratings()
                 ratings.sort(reverse=True)
 
-                dates = [r.date for r in ratings[4:]]
+                dates = [r.date for r in ratings]
+                running_dates = [r.date for r in ratings[4:]]
+                helpful_ratings = []
+
+                i = 0
+                while i < len(ratings):
+                    average = [r.rating for r in ratings[0: i + 1]]
+                    helpful_ratings.append(sum(average) / (i + 1))
+                    i += 1
 
                 i = 0
                 running_averages = []
@@ -103,15 +125,20 @@ class RateMyProfessor(commands.Cog):
                     running_averages.append(sum(average) / 5)
                     i += 1
 
-                plt.plot(dates, running_averages, color='blue')
-                plt.title('Running Average', fontsize=20)
+                plt.plot(running_dates, running_averages, color='blue')
+                plt.plot(dates, helpful_ratings, color='red')
+
+                plt.title(f'Ratings for {professor.name}', fontsize=15)
                 plt.ylabel('Rating', fontsize=12)
                 plt.xlabel('Date', fontsize=12)
                 plt.ylim(1.0, 5.0)
+                plt.legend(handles=[Patch(color='red', label='Average Rating'),
+                                    Patch(color='blue', label='Running Average Rating')])
                 plt.grid(True)
+
                 plt.savefig('graph.png')
                 plt.clf()
-                await ctx.send(file=discord.File('graph.png'))
+                await ctx.send(file=discord.File('graph.png'))"""
 
 
 def setup(bot):
