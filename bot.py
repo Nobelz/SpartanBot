b1c398545e3a16f547e9053d921625e29d6f4825
Nobel Zhou <nobelzhou19@gmail.com>
2021-01-28 19:25:43 -0500

    Added add role function that doesn't work

--- bot.py
+++ bot.py
@@ -255,7 +255,9 @@ async def on_message(message):
                 or 'zach' in message.content.lower() or 'zachary' in message.content.lower() or check_id_in_members(
                     member_id=231948123444871168, member_list=message.mentions) \
                 or 'alberto' in message.content.lower() or check_id_in_members(member_id=239538443133124609,
-                                                                               member_list=message.mentions):
+                                                                               member_list=message.mentions) \
+                or 'padma' in message.content.lower() or check_id_in_members(member_id=393942273680998401,
+                                                                             member_list=message.mentions):
             await message.add_reaction('🧠')
         await bot.process_commands(message)
                                    
